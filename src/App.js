8053c5de53dbaf713e2f2eb18ff3f212b3e5a36f
Lekhy <yatinl@amazon.com>
2019-11-12 15:57:03 -0500

    initial attempt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Welcome from './components/welcome/Welcome'
+import Clock from './components/clock/Clock'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Welcome  name ="Eric"/>
+      <Clock/>
     </div>
   );
 }
